3f4b0b1c3d36760e906f6d46faa6913848b8567f
aasmyb <m.a.elkholaey@gmail.com>
2022-09-28 11:58:07 +0200

    Added users route.

--- app.js
+++ app.js
@@ -82,12 +82,54 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined.',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined.',
+  });
+};
+
+const addNewUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined.',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined.',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is not yet defined.',
+  });
+};
+
 app.route('/api/v1/tours').get(getAllTours).post(addNewTour);
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(addNewUser);
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 app.listen(PORT_NUM, () => {
   console.log(`App is running on port ${PORT_NUM}`);
