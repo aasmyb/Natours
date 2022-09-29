57ec6d62fbce25184e3893277dbaeb16840485ba
aasmyb <m.a.elkholaey@gmail.com>
2022-09-29 14:17:21 +0200

    Updated tours model and loaded API data.

---
+++ dev-data/data/impoert-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './../../config.env' });
+const Tour = require('./../../models/tourModel');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('db connected! ✅');
+  });
+
+// Read file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data loaded successfully!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete data from DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data deleted successfully!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
