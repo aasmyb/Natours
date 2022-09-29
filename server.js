e653cbb85fabc0e9f7a75e358e5c87461eb8d371
aasmyb <m.a.elkholaey@gmail.com>
2022-09-29 11:03:53 +0200

    Added mongoose.

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('db connected!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Park Camper v2',
+  price: 999,
+});
+testTour
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log(`😡 ${err} 😡`));
 
 const PORT_NUM = process.env.PORT || 3000;
 app.listen(PORT_NUM, () => {
