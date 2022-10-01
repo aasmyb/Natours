545629dc10e2b2ebb0f2ef80fd73d63cc8584e9a
aasmyb <m.a.elkholaey@gmail.com>
2022-10-01 12:23:23 +0200

    Handled undefined routes.

--- app.js
+++ app.js
@@ -17,4 +17,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+  next();
+});
+
 module.exports = app;
