94b5989bce5180f09c60250ce6695385538dfb61
aasmyb <m.a.elkholaey@gmail.com>
2022-10-01 13:28:02 +0200

    Added global error handling middleware.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 // Middlewares to have access to the body
 app.use(express.json());
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
@@ -19,11 +21,10 @@ app.use('/api/v1/users', userRouter);
 
 // Unhandled routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
-  next();
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+// Global error handling middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
