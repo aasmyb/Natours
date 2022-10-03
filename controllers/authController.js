0bc287187a8cf18af9e7efd8e4ea7265b061fb35
aasmyb <m.a.elkholaey@gmail.com>
2022-10-03 20:26:45 +0200

    Added cookies.

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,8 +7,19 @@ const sendEmail = require('../utils/email');
 const crypto = require('crypto');
 
 const createSendToken = (user, statusCode, res, sendUser = false) => {
-  const token = user.signToken(user._id);
   const data = sendUser ? { user } : undefined;
+  const token = user.signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
