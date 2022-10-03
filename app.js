537dc32a491c7c15840f97b79bed9170cbc7a624
aasmyb <m.a.elkholaey@gmail.com>
2022-10-03 22:05:10 +0200

    Added noSQL/XSS sanitization security.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-const morgan = require('morgan');
-const app = express();
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
+const app = express();
 // Global middlewares
 // Set security for HTTP headers
 app.use(helmet());
@@ -23,8 +26,14 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// To have access to the body => body parser
+// Body parser => to have access to the body data => puts them in req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against QSS attacks
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
