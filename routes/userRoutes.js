115bab8b5d8495ee41b13aaa0683b64fb78a45f2
aasmyb <m.a.elkholaey@gmail.com>
2022-10-04 19:06:12 +0200

    Added /me endpoint.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,6 +15,12 @@ router.patch(
   authController.changePassword
 );
 
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser
+);
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
