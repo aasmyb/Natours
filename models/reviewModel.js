440f9ad061b6e9aa8408f3f532cc15a6944ac63c
aasmyb <m.a.elkholaey@gmail.com>
2022-10-05 13:43:08 +0200

    Prevented duplicate reviews.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Tour = require('./tourModel');
-const { raw } = require('express');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -34,6 +33,8 @@ const reviewSchema = new mongoose.Schema(
     toJSON: { virtuals: true },
   }
 );
+
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function (next) {
   // "this" refers to query
